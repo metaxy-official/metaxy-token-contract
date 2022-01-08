a4e27dce7170efe6328265c06961397a9f729b7c
adactor <adalie@metaxy.game>
2022-01-08 17:03:06 +0700

    (upgrade): mxy v2

--- contracts/token/MxyToken.sol
+++ contracts/token/MxyToken.sol
@@ -2,17 +2,22 @@
 
 pragma solidity 0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./BEP20.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MxyToken is Ownable, BEP20 {
-    constructor(address wallet, uint256 totalSupply)
-        Ownable()
-        BEP20("Mxy Token", "MXY")
-    {
-        _mint(wallet, totalSupply);
-        transferOwnership(wallet);
+contract MxyToken is Ownable, ERC20Burnable {
+    using SafeERC20 for IERC20;
+
+    mapping(address => bool) public whales;
+
+    bool public antiWhaleEnabled;
+    uint256 public antiWhaleTime;
+    uint256 public antiWhaleAmount;
+
+    constructor(address _ownerAddress, uint256 _totalSupply) Ownable() ERC20("MXY Token","MXY") {
+        _mint(_ownerAddress, _totalSupply);
+        transferOwnership(_ownerAddress);
     }
 
     /**
@@ -23,45 +28,89 @@ contract MxyToken is Ownable, BEP20 {
      *
      * - `msg.sender` must be the token owner
      */
-    function mint(uint256 amount) public onlyOwner returns (bool) {
+    function mint(uint256 _amount) public onlyOwner returns (bool) {
         uint256 totalSupply = totalSupply();
         require(
-            totalSupply + amount < 1500000000 ether,
+            totalSupply + _amount < 1500000000 ether,
             "MXY::mint: exceeding the permitted limits"
         );
-        _mint(_msgSender(), amount);
+        _mint(_msgSender(), _amount);
         return true;
     }
 
     /**
-     * @dev Destroys `amount` tokens from the caller.
+     * @notice set a whale's address
      *
-     * See {BEP20-_burn}.
+     * @param _account address
+     *
+     * Requirements
+     *
+     * - `msg.sender` must be the token owner
      */
-    function burn(uint256 amount) public {
-        _burn(_msgSender(), amount);
+    function setWhale(address _account) external onlyOwner {
+        require(!whales[_account], "MXY: account was set");
+
+        whales[_account] = true;
     }
 
     /**
-     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
-     * allowance.
+     * @notice enable anti whale
      *
-     * See {BEP20-_burn} and {BEP20-allowance}.
+     * @param _amount amount
+     * @param _duration duration
      *
-     * Requirements:
+     * Requirements
      *
-     * - the caller must have allowance for ``accounts``'s tokens of at least
-     * `amount`.
+     * - `msg.sender` must be the token owner
      */
-    function burnFrom(address account, uint256 amount) public {
-        uint256 currentAllowance = allowance(account, _msgSender());
-        require(
-            currentAllowance >= amount,
-            "BEP20: burn amount exceeds allowance"
-        );
-        unchecked {
-            _approve(account, _msgSender(), currentAllowance - amount);
+    function enableAntiWhale(uint256 _amount, uint256 _duration) external onlyOwner {
+        require(!antiWhaleEnabled, "MXY: anti whale was enabled");
+        require(_amount > 0, "MXY: amount is invalid");
+
+        antiWhaleEnabled = true;
+        antiWhaleAmount = _amount;
+        antiWhaleTime = block.timestamp + _duration;
+    }
+
+    /**
+     * @notice disable anti whale
+     *
+     * Requirements
+     *
+     * - `msg.sender` must be the token owner
+     */
+    function disableAntiWhale() external onlyOwner {
+        antiWhaleEnabled = false;
+        antiWhaleTime = 0;
+    }
+
+    /**
+     * @notice emergency withdraw token
+     *
+     * @param _token token address
+     * @param _to to address
+     * @param _amount amount
+     *
+     * Requirements
+     *
+     * - `msg.sender` must be the token owner
+     */
+    function emergencyWithdraw(IERC20 _token, address _to, uint256 _amount) external onlyOwner {
+       if (_token == IERC20(address(0))) {
+            (bool success, ) = _to.call{value: _amount}("");
+            require(success, "MXY: Transfer failed");
+        } else {
+            _token.safeTransfer(_to, _amount);
+        }        
+    }
+
+    function _transfer(address _sender, address _recipient, uint256 _amount) internal virtual override {
+        if (antiWhaleEnabled) {
+            if (antiWhaleTime > block.timestamp && _amount > antiWhaleAmount && whales[_sender]) {
+                revert("MXY: Anti Whale");
+            }
         }
-        _burn(account, amount);
+
+        super._transfer(_sender, _recipient, _amount);
     }
 }
